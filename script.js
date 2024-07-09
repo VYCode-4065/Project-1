cf5db03d44f00ca35597e0da9c38db3edf0fa84e
Vishal Yadav <yadavvishal4065@gmail.com>
2024-07-09 20:21:24 +0530

    v2.5

--- script.js
+++ script.js
@@ -30,20 +30,20 @@ var xscale = 1;
 var yscale = 1;
 var xprev = 0;
 var yprev = 0;
+var scrll = document.querySelector('.scroller');
 function MouseScroller(xscale, yscale) {
-    var scrll = document.querySelector('.scroller');
+    
     window.addEventListener("mousemove", (dets) => {
         scrll.style.left = dets.x + 'px';
         scrll.style.top = dets.y + 'px';
         scrll.style.transform = `scale(${xscale},${yscale})`;
-
-
     })
 }
 function SkewCircle() {
+    var timeout = 0;
     window.addEventListener('mousemove', (dets) => {
         clearTimeout(timeout);
-        xscale = gsap.utils.clamp(.4, 1.6, dets.clientX - xprev);
+        xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
         yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
         xprev = dets.clientX;
         yprev = dets.clientY;
@@ -54,6 +54,32 @@ function SkewCircle() {
         }, 100);
     })
 }
+
+var allElem = document.querySelectorAll('.elem');
+allElem.forEach((elem)=>{
+    var diff = 0;
+    var diffrote = 0;
+    var rotate = 0;
+    elem.addEventListener('mousemove',(dets)=>{
+        diff = dets.clientY-elem.getBoundingClientRect().top;
+        diffrote = gsap.utils.clamp(-15,15,(dets.clientX - rotate));
+        rotate = dets.clientX;
+        gsap.to(elem.querySelector('img'),{
+            opacity:1,
+            ease:Power3,
+            duration:.3,
+            top:diff,
+            rotate:diffrote,
+            left:dets.clientX
+        });
+    });
+    elem.addEventListener('mouseleave',(dets)=>{
+        gsap.to(elem.querySelector('img'),{
+            opacity:0,
+            ease:Power1
+        });
+    });
+});
 
 firstPageAnim();
 MouseScroller();
